ddedfdb8b4536b21e87a0bff03638a955cbe62d0
liuyang <906707001@qq.com>
2025-07-16 19:03:10 +0800

    删除菜品与修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -9,12 +9,28 @@ import java.util.List;
 
 @Mapper
 public interface DishFlavorMapper {
-//    批量插入口味数据
+    /**
+     * 批量插入口味数据
+     * @param flavors
+     */
     void insertBatch(List<DishFlavor> flavors);
 
-//    删除菜单关联的口味数据
+    /**
+     * 删除菜单关联的口味数据
+     * @param id
+     */
     void deleteByDishId(Long id);
 
-//    根据菜品id查询口味数据
+    /**
+     * 根据菜品id批量删除菜单关联的口味数据
+     * @param dishIds
+     */
+    void deleteByDishIds(List<Long> dishIds);
+
+    /**
+     * 根据菜品id查询口味数据
+     * @param id
+     * @return
+     */
     List<DishFlavor> getByDishId(Long id);
 }
